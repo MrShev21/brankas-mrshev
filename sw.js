d536576d0a1693a0e26d7c298baf7034b5327e90
MrShev21 <mr.shev21@gmail.com>
2026-08-10 14:09:02 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = "brankas-misteri-v2";
+const CORE = [
+  "./",
+  "./BRANKAS_MISTERI_100_FINAL_100.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(CORE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match("./BRANKAS_MISTERI_100_FINAL_100.html"));
+    })
+  );
+});
